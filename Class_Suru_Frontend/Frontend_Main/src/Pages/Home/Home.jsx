cbbbc04ae13158246c70973ffcc206fced5d1d0e
mahatasourav <mahatasourav35@gmail.com>
2025-01-31 21:09:54 +0530

    Home sec Updated

--- Class_Suru_Frontend/Frontend_Main/src/Pages/Home/Home.jsx
+++ Class_Suru_Frontend/Frontend_Main/src/Pages/Home/Home.jsx
@@ -1,47 +1,10 @@
 // This page is for Sourav
 import React from "react";
-import Style from "../../css/Home.module.css";
-import Image from "../../assets/BooksImages.png";
-import HomeMainLeftDownImg from "../../assets/HomemainLeftImg.png";
-import Button from "../../Components/Button/Button";
+
+import HomeSec1 from "./HomeSec1";
 
 const Home = () => {
-  return (
-    <div className={Style.HomeMain}>
-      {/* Left Side Contents */}
-      <div className={Style.HomeMainLeft}>
-        <h1>
-          তোমার <span>স্বপ্ন পূরণের</span> শুরু এখানেই!সঠিক{" "}
-          <span>প্রস্তুতি</span>, সেরা <span>সাফল্য</span>!
-        </h1>
-        <p>
-          তোমার <span>স্বপ্ন পূরণের</span> পথে <span>সেরা গাইডলাইন</span>, সঠিক
-          প্রস্তুতি এবং নির্ভরযোগ্য সহায়তা দিচ্ছি আমরা, যাতে{" "}
-          <span>সফল ভবিষ্যৎ </span>গড়া হয়ে ওঠে আরও সহজ এবং নিশ্চিত আমাদের
-          সঙ্গে এগিয়ে চলো!
-        </p>
-        <div className={Style.HomeMainLeftDown}>
-          <img
-            className={Style.HomeMainLeftDownImg}
-            src={HomeMainLeftDownImg}
-            alt="image"
-          />
-          <p>
-            বাংলার <span>1k+</span> শিক্ষার্থীর আস্থার ঠিকানা।
-          </p>
-        </div>
-        <div className={Style.HomeMainLeftButton}>
-          <Button text="Get Started" />
-          <Button text="About Us" />
-        </div>
-      </div>
-
-      {/* Right Side Image */}
-      <div>
-        <img className={Style.RightImg} src={Image} alt="Education" />
-      </div>
-    </div>
-  );
+  return <HomeSec1></HomeSec1>;
 };
 
 export default Home;

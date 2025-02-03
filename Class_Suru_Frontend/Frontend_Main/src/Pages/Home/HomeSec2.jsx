3765e6031e210f8af95583d92dbfccd58aac63fa
mahatasourav <mahatasourav35@gmail.com>
2025-02-03 23:02:32 +0530

    home sec2 added

--- Class_Suru_Frontend/Frontend_Main/src/Pages/Home/HomeSec2.jsx
+++ Class_Suru_Frontend/Frontend_Main/src/Pages/Home/HomeSec2.jsx
@@ -1,12 +1,52 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import imageData from "../../assets/sildeimage";
+import Style from "../../css/Home.module.css";
+
 const HomeSec2 = () => {
   return (
-    <>
-      <div>
-        <center>
-          <h1>HIIIIIIII, I Am Sec 2</h1>
-        </center>
+    <div className={Style.HomeSec2div}>
+      <div className="max-w-full mx-auto py-10 ">
+        <Swiper
+          effect="coverflow"
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={2} // Always show 2 slides
+          spaceBetween={10}
+          coverflowEffect={{
+            rotate: 30,
+            stretch: 0,
+            depth: 200,
+            modifier: 2,
+            slideShadows: true,
+          }}
+          pagination={{ clickable: true }}
+          navigation={true} // Enable next/prev buttons
+          modules={[EffectCoverflow, Pagination, Navigation]}
+          className="custom-swiper"
+        >
+          {imageData.map((image) => (
+            <SwiperSlide key={image.id} className={Style.customSlide}>
+              <div className={Style.Swiperdiv}>
+                <img
+                  src={image.src} // Dynamically use the src from imageData.js
+                  alt={image.alt}
+                  loading="lazy" // Lazy loading for performance
+                />
+                <div>
+                  <h3>{image.name}</h3>
+                  <p>{image.college}</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
-    </>
+    </div>
   );
 };
 
---
+++ Class_Suru_Frontend/Frontend_Main/src/assets/sildeimage.js
@@ -0,0 +1,43 @@
+// imageData.js
+import DebdulalImg from './DebdulalImg.png'; // Import the image
+
+const imageData = [
+  { 
+    src: DebdulalImg, 
+    alt: 'DebdulalImg', 
+    name: 'Debdulal', // Add name
+    college: 'XYZ University' // Add college
+  },
+  { 
+    src: DebdulalImg, 
+    alt: 'DebdulalImg', 
+    name: 'Amit', 
+    college: 'ABC College'
+  },
+  { 
+    src: DebdulalImg, 
+    alt: 'DebdulalImg', 
+    name: 'Priya', 
+    college: 'DEF Institute'
+  },
+  { 
+    src: DebdulalImg, 
+    alt: 'DebdulalImg', 
+    name: 'Raj', 
+    college: 'LMN University'
+  },
+  { 
+    src: DebdulalImg, 
+    alt: 'DebdulalImg', 
+    name: 'Ankita', 
+    college: 'PQR College'
+  },
+  { 
+    src: DebdulalImg, 
+    alt: 'DebdulalImg', 
+    name: 'Sourav', 
+    college: 'STU University'
+  }
+];
+
+export default imageData;

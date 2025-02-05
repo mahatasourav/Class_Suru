fb322b43e82652442f9aaf1ec0be48069607ec68
youtanimstar <animdeep2019@gmail.com>
2025-02-05 21:25:04 +0530

    Add Navbar component and update styles; enhance layout with margin adjustments

--- Class_Suru_Backend/backend_main/models/userModel.js
+++ Class_Suru_Backend/backend_main/models/userModel.js
@@ -27,11 +27,9 @@ pool.connect((err, client, release) => {
 const createUser = async (email, username, hashedPassword, phoneNumber) => {
   try {
     const result = await pool.query(
-      "INSERT INTO public.users (username, email, password,phone_number) VALUES ($1, $2, $3) RETURNING id",
-      [username, email, hashedPassword,phoneNumber] 
+      "INSERT INTO public.users (name, email, password, phone_number) VALUES ($1, $2, $3, $4) RETURNING id",
+      [username, email, hashedPassword, phoneNumber] 
     );
-    const display = await pool.query("SELECT * FROM users ORDER BY id ASC ");
-    console.log('display',display);
     
     return result.rows[0];
   } catch (error) {

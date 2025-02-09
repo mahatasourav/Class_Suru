967b6eeb70ab0d0c7ef4b06e598f68c63d43f010
mahatasourav <mahatasourav35@gmail.com>
2025-02-09 16:23:55 +0530

    HomeSec4 added

--- Class_Suru_Frontend/Frontend_Main/src/Pages/Home/Home.jsx
+++ Class_Suru_Frontend/Frontend_Main/src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Style from "../../css/Home.module.css";
 import HomeSec1 from "./HomeSec1";
 import HomeSec2 from "./HomeSec2";
 import HomeSec3 from "./HomeSec3";
+import HomeSec4 from "./HomeSec4";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
         <HomeSec1 />
         <HomeSec2 />
         <HomeSec3 />
+        <HomeSec4 />
       </div>
     </>
   );

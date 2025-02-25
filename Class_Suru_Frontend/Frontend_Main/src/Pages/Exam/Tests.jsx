057afbc2fd4fee0e136d7c099b1f540e5ab159bf
mahatasourav <mahatasourav35@gmail.com>
2025-02-25 17:27:30 +0530

    Add subject tests data and enhance exam navigation UI

--- Class_Suru_Frontend/Frontend_Main/src/Pages/Exam/Tests.jsx
+++ Class_Suru_Frontend/Frontend_Main/src/Pages/Exam/Tests.jsx
@@ -3,29 +3,56 @@ import { Link, useParams } from "react-router-dom";
 import Style from "../../css/Exam.module.css";
 import Breadcrumb from "./Breadcrumb";
 import { subjectTests } from "../../assets/ExamData/subjectTests";
-
+import Button from "../../Components/Button/Button";
+import { FaFileCircleQuestion } from "react-icons/fa6";
+import { TfiTimer } from "react-icons/tfi";
+import { TbTargetArrow } from "react-icons/tb";
 const Tests = () => {
   const { examName, subjectName } = useParams(); // Get exam & subject from URL
   const Tests = subjectTests[subjectName] || []; // Get modules or empty array
 
   return (
-    <div className={Style.Modules}>
-      <Breadcrumb /> {/* Breadcrumb Navigation */}
-      <h2>{subjectName} - Test</h2>
-      <ul>
+    <div className={Style.TestSection}>
+      <div>
+        <Breadcrumb />
+        {/* <input type="checkbox" />
+        All */}
+      </div>{" "}
+      {/* Breadcrumb Navigation */}
+      <div className={Style.Tests}>
         {Tests.length > 0 ? (
           Tests.map((Test, index) => (
-            <li key={index}>
+            <p key={index} className={Style.Test}>
+              <div>
+                <h2> {Test}</h2>
+                <p className={Style.TestLower}>
+                  <span>
+                    <FaFileCircleQuestion />
+                    75 Question{" "}
+                  </span>
+                  <span>
+                    <TfiTimer />
+                    180 minutes
+                  </span>
+                  <span>
+                    <TbTargetArrow />
+                    300 Marks
+                  </span>
+                </p>
+              </div>
+
               <Link to={`/exam/${examName}/${subjectName}/instruction`}>
-                {Test}
+                <Button text="Start Exam" />
               </Link>
-            </li>
+            </p>
           ))
         ) : (
           <p>No modules found for this subject.</p>
         )}
-      </ul>
-      <Link to={`/exam/${examName}`}>🔙 Back to {examName} Subjects</Link>
+      </div>
+      {/* <Link to={`/exam/${examName}`} className={Style.BackToPrev}>
+        🔙{" "}
+      </Link> */}
     </div>
   );
 };

4eaee1a34c7bc4423f47b3405ad64ff22e1bc9ed
mahatasourav <mahatasourav35@gmail.com>
2025-04-14 18:51:54 +0530

    Add About page with faculty details and responsive design; implement Swiper for faculty images; create about.module.css for styling.

---
+++ Class_Suru_Frontend/Frontend_Main/src/assets/facultyData.js
@@ -0,0 +1,70 @@
+// imageData.js
+import DebdulalImg from "./DebdulalImg.png"; // Import the image
+import SamirImg from "./SamirImg.png";
+import ApurboImg from "./ApurbaImg.png";
+
+const imageData = [
+  {
+    src: DebdulalImg,
+    alt: "DebdulalImg",
+    name: "Debdulal Dey", // Add name
+    Description: "Physic Faculty Member ( IIT KGP , 3rd year student )", // Add college
+  },
+
+  {
+    src: DebdulalImg,
+    alt: "DebdulalImg",
+    name: "Debdulal", // Add name
+    Description: "Physic Faculty Member ( IIT KGP , 3rd year student )", // Add college
+  },
+  {
+    src: DebdulalImg,
+    alt: "DebdulalImg",
+    name: "Debdulal", // Add name
+    Description:
+      "Chemsistry Faculty Memeber(Mbbs, 3rd year students, rg kar medical collage )", // Add college
+  },
+  {
+    src: DebdulalImg,
+    alt: "DebdulalImg",
+    name: "Debdulal", // Add name
+    Description: "Physic Faculty Member ( IIT KGP , 3rd year student )", // Add college
+  },
+  {
+    src: DebdulalImg,
+    alt: "DebdulalImg",
+    name: "Debdulal", // Add name
+    Description:
+      "Chemsistry Faculty Memeber(Mbbs, 3rd year students, rg kar medical collage )", // Add college
+  },
+  {
+    src: DebdulalImg,
+    alt: "DebdulalImg",
+    name: "Debdulal", // Add name
+    Description:
+      "Chemsistry Faculty Memeber(Mbbs, 3rd year students, rg kar medical collage )", // Add college
+  },
+  {
+    src: DebdulalImg,
+    alt: "DebdulalImg",
+    name: "Debdulal", // Add name
+    Description:
+      "Chemsistry Faculty Memeber(Mbbs, 3rd year students, rg kar medical collage )", // Add college
+  },
+  {
+    src: DebdulalImg,
+    alt: "DebdulalImg",
+    name: "Debdulal", // Add name
+    Description:
+      "Chemsistry Faculty Memeber(Mbbs, 3rd year students, rg kar medical collage )", // Add college
+  },
+  {
+    src: DebdulalImg,
+    alt: "DebdulalImg",
+    name: "Debdulal", // Add name
+    Description:
+      "Chemsistry Faculty Memeber(Mbbs, 3rd year students, rg kar medical collage )", // Add college
+  },
+];
+
+export default imageData;

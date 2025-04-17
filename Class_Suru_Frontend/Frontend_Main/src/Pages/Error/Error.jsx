8276c953989e3dda3bf3a9a85c76564627511734
youtanimstar <animdeep2019@gmail.com>
2025-04-17 15:30:25 +0530

    error page added and updated

--- Class_Suru_Frontend/Frontend_Main/src/Pages/Error/Error.jsx
+++ Class_Suru_Frontend/Frontend_Main/src/Pages/Error/Error.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { useRouteError } from "react-router-dom";
+import errorImg from "../../assets/404.png"
+import Style from "../../css/error.module.css"
+import { Button } from "../../Components";
 
 const Error = () => {
   const error = useRouteError();
   return (
-    <>
-      <h1>Someting Wents Wrong</h1>
-      <p>{error || "An unexpected error occurred"}</p>
-    </>
+    <div className={Style.error}>
+      <img className={Style.errorimg} src={errorImg} alt='' />
+      <h1 className={Style.errorHeading}>Someting Wents Wrong</h1>
+      <p className={Style.errorMessage}>{error || "An unexpected error occurred"}</p>
+      <Button isLink={true} link="/" text="Go To Home" />
+    </div>
   );
 };
 
